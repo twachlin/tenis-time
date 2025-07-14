3372c78e427a1031c584008d54c7669bf87c6896
twachlin <tomaswachlin@gmail.com>
2025-07-14 18:49:10 -0300

    First commit

---
+++ app/src/main/java/com/example/tenis_time/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package com.example.tenis_time.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun TenistimeTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
